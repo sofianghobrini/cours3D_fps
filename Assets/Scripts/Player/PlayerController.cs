58240c4d6411d23e7aa40324cc6534153a443151
sofianghobrini <sofiangho31000@outlook.fr>
2026-04-24 15:29:58 +0200

    cours 2 mouvements du joueur

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,7 +7,13 @@ public class PlayerController : MonoBehaviour
     
     
     [SerializeField]
-   private float speed;
+   private float speed = 3f;
+
+    [SerializeField]
+   private float mouseSensitivityX = 3f;
+
+   [SerializeField]
+   private float mouseSensitivityY = 10f;
 
 
    private PlayerMotor motor;
@@ -25,8 +31,25 @@ public class PlayerController : MonoBehaviour
         float zMov = Input.GetAxisRaw("Vertical");
 
         Vector3 moveHorizontal = transform.right * xMov;
-        Vector3 moveVertical = transform.right * zMov;
+        Vector3 moveVertical = transform.forward * zMov;
 
         Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+
+        motor.Move(velocity);
+
+
+        // On calcul la rotation du joueur en vector 3
+        float yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 rotation = new Vector3(0f, yRot, 0f) * mouseSensitivityX;
+
+        motor.Rotate(rotation);
+
+        // On calcul la rotation de la camera en vector 3
+        float xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 cameraRotation = new Vector3(xRot, 0f, 0f) * mouseSensitivityY;
+
+        motor.RotateCamera(cameraRotation);
     }
 }
--- Assets/Scripts/Player/PlayerMotor.cs
+++ Assets/Scripts/Player/PlayerMotor.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMotor : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private Camera cam;
+    private Vector3 velocity;
+    private Vector3 rotation;
+    private Vector3 cameraRotation;
+    private Rigidbody rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move(Vector3 _velocity)
     {
-        
+        velocity = _velocity;
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    public void RotateCamera(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation;
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    private void PerformMovement()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    private void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        cam.transform.Rotate(-cameraRotation);
     }
 }
