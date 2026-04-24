53cd1096ea5c77b961ee8cb9fe69a587e6f8af2f
sofianghobrini <sofiangho31000@outlook.fr>
2026-04-24 14:26:02 +0200

    cours 1

---
+++ Assets/Scripts/Player/PlayerMotor.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
